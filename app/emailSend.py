08d6652742655c95d6448fdcf703ae716e0eeb61
Grant Boicheff <gboicheff@gmail.com>
2020-07-10 12:50:36 -0400

    Update emailSend.py
    Changed imports

--- app/emailSend.py
+++ app/emailSend.py
@@ -1,7 +1,6 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
 from secrets import *
 import os
