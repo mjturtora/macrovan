8bcb2a88c791d046404c6726d11939d0ad46dcfa
Grant Boicheff <gboicheff@gmail.com>
2020-07-17 15:26:34 -0400

    Update emailSend.py
    PDF file search now ignores spaces

--- app/emailSend.py
+++ app/emailSend.py
@@ -20,10 +20,11 @@ testMode = True
 def sendAllEmails():
     sendEmails(emailsAndDirPaths.keys())
 
-
+#sendEmails to a list of organizers
 def sendEmails(organizers):
     #Iterate through the organizer email address and email them their specific zip file
     numFiles = 0
+    expectedFileCount = 0
     if not testMode:
         session = smtplib.SMTP('smtp.gmail.com', 587) 
         session.starttls() 
@@ -31,7 +32,7 @@ def sendEmails(organizers):
         #Iterate through each organizer creating their emails and calling the attachPDFs function to attach their files
         for organizer in organizers:
             organizerTurfCount = len(turf_dict[organizer])
-
+            expectedFileCount += organizerTurfCount
             #Create the email        
             message = MIMEMultipart()
             message['From'] = emailAddress
@@ -43,11 +44,12 @@ def sendEmails(organizers):
             message['To'] = email
             receiver_address = email
             message.attach(MIMEText(emailBody, 'plain'))
-            text = message.as_string()
 
             #attachPDFs attaches the PDFs and returns the number of PDFs it attached
             numAttachedFiles = attachPDFs(organizer, message) 
             numFiles += numAttachedFiles
+
+            text = message.as_string()
             
             #Check that the organizer had all of their files attached.  The email will not be sent if all the files were not attached
             if(numAttachedFiles == organizerTurfCount and numAttachedFiles != 0):
@@ -58,8 +60,16 @@ def sendEmails(organizers):
                 else:
                     print("Email sent to: " + fullName + " : " + str(numAttachedFiles) + " files successfully attached")
             else:
-                print("Email failed to send to: " + fullName)               
+                print("Email failed to send to: " + fullName)
+            print()               
         session.quit()
+        #Compare the total count of attached files to the amount of turfs    
+        if numFiles == expectedFileCount:
+            print("All " + str(numFiles) + " files successfully attached!")
+        else:
+            difference = expectedFileCount - numFiles
+            print(expectedFileCount)
+            print("Failed to attach " + str(difference) + " files.....")
 
     #In test mode the files will still be searched for and found filenames will be displayed
     else:
@@ -74,24 +84,21 @@ def sendEmails(organizers):
             numFiles += attachPDFs(organizer,0)
             print("=========================================")
 
-    #Compare the total count of attached files to the amount of turfs
-    expectedFileCount = len(getAllTurfs(turf_dict))
-    if numFiles == expectedFileCount:
-        print("All " + str(numFiles) + " files successfully attached!")
-    else:
-        difference = expectedFileCount - numFiles
-        print(expectedFileCount)
-        print("Failed to attach " + str(difference) + " files.....")
 
+#Ignore spaces in the filename
 def attachPDFs(organizer, message):
     numFiles = 0
     for file in turf_dict[organizer]:
+        expectedFileName = file[0] + " " + file[1] + ".pdf"
         fileName = file[0] + " " + file[1] + "*" + ".pdf"
+        fileName = fileName.replace(" ", "")
         outFileName= file[0] + " " + file[1] + ".pdf"
         if not testMode:  
             #Search the directory for a file that matches the fileName
             for file in os.listdir(path):
-                if fnmatch.fnmatch(file, fileName):
+                foundFile = file.replace(" ", "")
+                if fnmatch.fnmatch(foundFile, fileName):
+                    print(file)
                     pdf = MIMEApplication(open(path + file, 'rb').read())
                     pdf.add_header('Content-Disposition','attachment', filename=outFileName)
                     message.attach(pdf)
@@ -102,18 +109,19 @@ def attachPDFs(organizer, message):
         else:
             foundFile = "FILE NOT FOUND-------------------------------------F"
             for file in os.listdir(path):
-                if fnmatch.fnmatch(file, fileName):
-                    foundFile = file
+                foundFile = file.replace(" ", "")
+                if fnmatch.fnmatch(foundFile, fileName):
+                    foundFileName = file
                     test = open(path + file, 'rb')
                     test.close()
                     numFiles+=1
                     break
             #Expected on left, found on right            
-            print(fileName + " : " + foundFile)
+            print(expectedFileName + " : " + foundFileName)
     return numFiles
 
     
 
 if __name__ == '__main__':    
     #sendAllEmails()
-    sendEmails([("Kate", "Steinway")])
+    #sendEmails([("Kate", "Steinway"), ("Charles", "Walston")])
