ad30a98ff6296a1fbf425899a6a2f30dbac12d47
mjturtora <mjturtora@gmail.com>
2020-08-31 11:11:52 -0400

    Outputs to excel

--- app/read_pdf.py
+++ app/read_pdf.py
@@ -5,6 +5,7 @@
 
 import PyPDF2
 import os
+import pandas as pd
 
 def get_fnames(path):
     # Get all the PDF filenames.
@@ -17,13 +18,31 @@ def get_fnames(path):
     #print(pdf_files)
     return pdf_files
 
+
+def excel_write(path, lists):
+    """export to excel worksheet"""
+    df = pd.DataFrame({'List Name': [element[0] for element in lists],
+                       'List Number': [element[1] for element in lists]})
+    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='List Numbers', index=False)
+    writer.save()
+    writer.close()
+
 if __name__ == '__main__':
     # Loop through all the PDF files.
     path = r'D:\Stuff\Projects\Pol\macrovan\io\Output'
     pdf_files = get_fnames(path)
+    lists = []
     for filename in pdf_files:
-        print(filename)
+        #print(filename)
         pdfFileObj = open(r'D:\Stuff\Projects\Pol\macrovan\io\Output\\' + filename, 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         page = pdfReader.getPage(2).extractText()
-        print(page.split("List", 1)[1])
+        lname, lnum = page.split("List", 1)
+        lists.append([lname, lnum])
+
+    print([element[1] for element in lists])
+    write_path = r'..\io\Output\List Numbers.xlsx'
+    excel_write(write_path, lists)
+
+
