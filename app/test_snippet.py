ef6f0f413a0e764c422bc729db2f4ce6323b6c42
mjturtora <mjturtora@gmail.com>
2020-09-21 10:46:27 -0400

    str.translate test in test_snippet.py

--- app/test_snippet.py
+++ app/test_snippet.py
@@ -1,15 +1,5 @@
 from utils import *
 
-""" keep code block for pop-up reference 
-import ctypes  # An included library with Python install.
-ctypes.windll.user32.MessageBoxW(0, "Click Ok to Finish", "Macrovan", 1)
-
-# import ctypes  # An included library with Python install.
-def Mbox(title, text, style):
-     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
-
-Mbox('Your title', 'Your text', 1)
-"""
-
-get_os()
-
+print_list_name = 'H.E!L\L*O'
+print_list_name = print_list_name.translate(print_list_name.maketrans('', '', '.!*\\'))
+print(print_list_name)
