c4c234a249345c50736f9cfb0839f93037a8f723
Grant Boicheff <gboicheff@gmail.com>
2021-05-07 14:58:39 -0400

    more progress on search_terms.py

--- app/search_terms.py
+++ app/search_terms.py
@@ -1,6 +1,6 @@
 from utils import *
-
-
+from bs4 import BeautifulSoup
+import time
 path = os.getcwd()
 print(f"The current working directory is {path}")
 
@@ -12,9 +12,41 @@ if __name__ == '__main__':
     driver.implicitly_wait(10)
     attempt_login(driver)  
     list_folders(driver)
-    select_folder(driver, '//*[text()="Testing"]')
+    select_folder(driver, '//*[text()="*2021 Municipal St. Petersburg Master"]')
     table = expect_by_tag(driver, "table")
+
+
     row = select_row(driver, table)
-    tbody = expect_by_tag(row, "tbody")
-    edit_button = expect_by_tag(row, "a")
+    button = expect_by_XPATH(expect_by_tag(row, "tbody"), '//*[@id="ctl00_ContentPlaceHolderVANPage_gvList"]/tbody/tr[1]/td[4]')
+    edit_button = expect_by_tag(button, "a")
     edit_button.click()
+
+    # accept alert
+    time.sleep(5)
+    driver.switch_to.alert.accept()
+
+    button = expect_by_XPATH(driver, '//*[@id="addStep"]')
+    button.click()
+    inner_button = expect_by_XPATH(button, '//*[@id="editSearchOption"]')
+    inner_button.click()
+
+    # select new table
+    table = expect_by_XPATH(driver, '//*[@id="ctl00_ContentPlaceHolderVANPage_AllPageSectionsPanelHolder"]')
+
+    rows = table.find_elements(By.CLASS_NAME, "OpenClosePSButton")
+    for row in rows:
+        src = row.get_attribute("src")
+        if "right" in src:
+            time.sleep(1)
+            try:
+                row.click()
+            except Exception:
+                pass
+    
+    # parse the selected options
+    page_src = driver.page_source
+    soup = BeautifulSoup(page_src, 'html.parser')
+
+    with open("src.txt", "w", encoding="utf-8") as file:
+        file.write(str(soup))
+
