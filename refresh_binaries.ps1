ab977a43a2b7576e182444175de154891ea10a92
mturtora <mike@turtora.net>
2026-04-03 23:11:22 -0400

    add -v2 to refresh_binaries.yml

--- refresh_binaries.ps1
+++ refresh_binaries.ps1
@@ -1,5 +1,5 @@
 # Trigger the build
-gh workflow run build-executables.yml --ref dev
+gh workflow run build-executables-v2.yml --ref dev
 
 # Wait for completion (Select the top run if prompted)
 gh run watch
